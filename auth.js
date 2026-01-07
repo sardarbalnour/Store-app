11e9c94e5fc5c5c75781780b0b0b9e6f5aaef255
Sardar Balnour <balnour.sardar@gmail.com>
2026-01-07 22:23:04 +0330

    validation

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import { postData } from "./utils/httpReq.js";
 import { setCookie } from "./utils/cookie.js";
 import authHandler from "./utils/authorizathion.js";
+import validateForm from "./utils/validation.js";
 
 const inputBox = document.querySelectorAll("input");
 const loginButton = document.querySelector("button");
@@ -12,6 +13,10 @@ const submitHandler = async (event) => {
 
   const username = inputBox[0].value;
   const password = inputBox[1].value;
+
+  const validation = validateForm(username, password);
+  if (!validation) return;
+
   const data = { username, password };
   const response = await postData("auth/login", data);
   setCookie(response.token);
