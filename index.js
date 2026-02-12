3b1a31cabc08a25b9e20d189b5c8d9c32387bceb
Sardar balnour <balnour.sardar@gmail.com>
2026-02-12 23:04:30 +0330

    showProducts function

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 import { getCookie } from "./utils/cookie.js";
 import { getData } from "./utils/httpReq.js";
+import { shortenText } from "./utils/stringFunc.js";
 
 const loginButton = document.getElementById("login");
 const dashboardButton = document.getElementById("dashboard");
+const mainContent = document.getElementById("products");
+
+const showProducts = (products) => {
+  mainContent.innerHTML = "";
+  products.forEach((product) => {
+    const jsx = `
+      <div>
+        <img alt=${product.title} src=${product.image} />
+        <h4>${shortenText(product.title)}</h4>
+
+        <div id="price">
+          <p>$ ${product.price}</p>
+          <button>
+            Buy
+            <i class="fa-solid fa-cart-shopping"></i>
+          </button>
+        </div>
+
+        <div id="rate">
+            <i class="fa-solid fa-star"></i>
+            <span>${product.rating.rate}</span>
+        </div>
+
+        <div id="count">
+            <i class="fa-solid fa-user"></i>
+            <span>${product.rating.count}</span>
+        </div>
+        
+      </div>
+    `;
+    mainContent.innerHTML += jsx;
+  });
+};
 
 const initHandler = async () => {
   const cookie = getCookie();
@@ -14,7 +48,7 @@ const initHandler = async () => {
   }
 
   const allProducts = await getData("products");
-  console.log(allProducts);
+  showProducts();
 };
 
 document.addEventListener("DOMContentLoaded", initHandler);
