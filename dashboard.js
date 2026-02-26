7f7dd9a10b21ab6650d4ce294db6431aa5c0b626
Sardar balnour <balnour.sardar@gmail.com>
2026-02-26 12:28:06 +0330

    dashboard style

--- dashboard.js
+++ dashboard.js
@@ -9,33 +9,35 @@ const renderUsers = (users) => {
   users.forEach((user) => {
     const jsx = `
       <div id="card">
-        <h4>${user.id}</h4>
-      </div>
+        <h3>${user.id}</h3>
 
-      <div>
-        <p><i class="fa-solid fa-user"></i>Name:</p>
-        <span>${user.name.firstname} ${user.name.lastname}</span>
-      </div>
+        <div>
+          <p><i class="fa-solid fa-user"></i>Name:</p>
+          <span>${user.name.firstname} ${user.name.lastname}</span>
+        </div>
 
-      <div>
-        <p><i class="fa-solid fa-paperclip"></i>Username:</p>
-        <span>${user.username}</span>
-      </div>
+        <div>
+          <p><i class="fa-solid fa-paperclip"></i>Username:</p>
+          <span>${user.username}</span>
+        </div>
 
-      <div>
-        <p><i class="fa-solid fa-envelope"></i>Email:</p>
-        <span>${user.email}</span>
-      </div>
+        <div>
+          <p><i class="fa-solid fa-envelope"></i>Email:</p>
+          <span>${user.email}</span>
+        </div>
 
-      <div>
-        <p><i class="fa-solid fa-phone"></i>Phone:</p>
-        <span>${user.phone}</span>
-      </div>
+        <div>
+          <p><i class="fa-solid fa-phone"></i>Phone:</p>
+          <span>${user.phone}</span>
+        </div>
 
-      <div>
-        <p><i class="fa-solid fa-location-dot"></i>Address:</p>
-        <span>${user.address.city} - ${user.address.street} - ${user.address.zipcode}</span>
-      </div>
+        <div>
+          <p><i class="fa-solid fa-location-dot"></i>Address:</p>
+          <span>${user.address.city} - ${user.address.street} - ${user.address.zipcode}</span>
+        </div>
+     </div>
+
+
     `;
 
     mainContent.innerHTML += jsx;
